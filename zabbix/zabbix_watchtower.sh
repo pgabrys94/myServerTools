b2dc2cbb28dde37a44460ac17323fb205800142a
Paweł Gabryś <141257739+pgabrys94@users.noreply.github.com>
2023-08-16 20:35:20 +0200

    Update zabbix_watchtower.sh
    Simplicity at its best

--- zabbix/zabbix_watchtower.sh
+++ zabbix/zabbix_watchtower.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
+mlup="$(docker logs --since "$since" watchtower 2>&1 | tail -n 1 | grep -P '[0-9]+(?=\:[0-9]+[A-Z])')"
+mdelay=2
+
 since="$(date -I)"
-since+="T00:00:00Z"
+since+="T00:""$mlup"+="$mdelay"":00Z"
 counter="$(docker logs --since "$since" watchtower 2>&1 | grep -c "Creating")"
-ecounter="$counter"
-container=0
-pos=0
 
 if [ "$counter" -eq 0 ]
         then echo "No recent updates"
-else {
 
-        while [ "$counter" -ne 0 ]; do
-                ((++pos))
+        else echo "$(docker logs --since "$since" watchtower 2>&1 | grep -oP '(?<=Creating\s\/)(.*)(?=")')"
 
-        declare "container[$pos]"="$(docker logs --since "$since" watchtower 2>&1 | grep -oP '(?<=Creating\s\/)(.*)(?=")' | tail -n "$counter" | head -1 )"
-
-                ((counter--))
-        done
-
-        while [ "$ecounter" -ne 0 ]; do
-
-                echo "${container["$ecounter"]}"
-
-                ((ecounter--))
-        done
-} fi
+fi
